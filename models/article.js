f7908b098175784dacb5f56d3de2deb7cb514819
Doreen Wiggins <Wiggy77@aol.com>
2018-02-25 13:44:02 -0500

    adding schema

---
+++ models/article.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const articleSchema = new Schema({
+  title: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+  url: { type: String, required: true }
+});
+const Book = mongoose.model("Article", articleSchema);
+module.exports = Article;
